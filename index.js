7d3ae1fc2dc55f3b840917e4a34630ce9f56a7cd
catherine <catmorillo@hotmail.com>
2023-02-13 13:12:33 -0500

    pushing

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+const topContainer= document.querySelector("#top-container") 
+const margaritaDetails = document.querySelector("#margarita-details")
+
+
+
 fetch("http://localhost:3000/drinks")
 .then(r => r.json())
-.then(data => console.log(data))
+.then(drinksArray => {
+    drinksArray.forEach(drink => 
+    
+
+renderDrinks(drink) 
+)}
+)
+
+function renderDrinks(drinkObj){
+
+
+
+const image = document.createElement("img")
+image.src = drinkObj.strDrinkThumb
+topContainer.append(image)
+image.addEventListener("click", () => {renderDetails(drinkObj)}
+
+)}
+
+function renderDetails(drinkObj){
+const h1= document.getElementsByClassName("name")
+const ingredients= document.getElementsByClassName("ingredients")
+const instructions=document.getElementsByClassName("instructions")
+const glass=document.getElementsByClassName ("glass")
+const image= document.getElementsByClassName("image")
+
+
+h1.innerText= drinkObj.strDrink
+ingredients.innerText= ""
+instructions.innerText=drinkObj.strInstructions
+glass.innerText=drinkObj.strGlass
+image.src=drinkObj.strDrinkThumb
+
+
+
+
+
+
+}

ff56b2a8f273cde9b319abfe3c482a3cb37af5e8
André Vargas Roo <a.vargas252@icloud.com>
2023-02-14 11:05:34 -0500

    cleaned up js, started css styling

--- index.js
+++ index.js
@@ -1,56 +1,47 @@
-
+// 
 const topContainer= document.querySelector("#top-container") 
 const margaritaDetails = document.querySelector("#margarita-details")
 const margForm = document.querySelector('form')
 
 
 fetch("http://localhost:3000/drinks")
-.then(r => r.json())
-.then(drinksArray => {
-    renderDetails(drinksArray[0])
-    drinksArray.forEach(drink => 
-    renderDrinks(drink) 
-    )})
+    .then(r => r.json())
+    .then(drinksArray => {
+        renderDetails(drinksArray[0])
 
-
-    
-
-
-
+        drinksArray.forEach(drink => 
+        renderDrinks(drink) 
+)})
 
 function renderDrinks(drinkObj){
+    const image = document.createElement("img")
+    image.src = drinkObj.strDrinkThumb
+    topContainer.append(image)
 
-const image = document.createElement("img")
-image.src = drinkObj.strDrinkThumb
-topContainer.append(image)
-image.addEventListener("click", () => {renderDetails(drinkObj)})
-image.addEventListener('mouseover', () => {
-image.className = 'onMouseover'
-})
+    image.addEventListener("click", () => {renderDetails(drinkObj)})
+    image.addEventListener('mouseover', () => {
+        image.className = 'onMouseover'}
+    )
 }
 
 function renderDetails(drinkObj){
-const h2= document.querySelector(".name")
-const instructions=document.querySelector(".instructions")
-const glass=document.querySelector (".glass")
-const image= document.querySelector(".image")
-const ingredients= document.querySelector(".ingredients")
+    const h2= document.querySelector(".name")
+    const instructions=document.querySelector(".instructions")
+    const glass=document.querySelector (".glass")
+    const image= document.querySelector(".image")
+    const ingredients= document.querySelector(".ingredients")
 
-h2.innerText= drinkObj.strDrink
-instructions.innerText=drinkObj.strInstructions
-glass.innerText=drinkObj.strGlass
-image.src=drinkObj.strDrinkThumb
+    h2.innerText= drinkObj.strDrink
+    instructions.innerText=drinkObj.strInstructions
+    glass.innerText=drinkObj.strGlass
+    image.src=drinkObj.strDrinkThumb
 
-
-
-ingredients.innerHTML=""
-drinkObj.strIngredient1.forEach(ingredient => {
-    const li = document.createElement("li")
-    li.innerText= ingredient
-    ingredients.append(li)
-    
-})
-
+    ingredients.innerHTML=""
+    drinkObj.strIngredient1.forEach(ingredient => {
+        const li = document.createElement("li")
+        li.innerText= ingredient
+        ingredients.append(li)
+    })
 }
 
 margForm.addEventListener('submit', (e) => {
@@ -66,20 +57,17 @@ margForm.addEventListener('submit', (e) => {
 
     renderDrinks(newDrinkObj)
     margForm.reset()
-
 })
 
 let addMarg = false
 const addButton = document.getElementById('add-new-marg')
 addButton.addEventListener('click', () => {
- addMarg = !addMarg
- if (addMarg) {
-    margForm.style.display = "block"
- }
- else {
-    margForm.style.display = "none"
- }
-} )
+    addMarg = !addMarg
+    if (addMarg) {
+        margForm.style.display = "block"}
+    else {
+        margForm.style.display = "none"}
+})
 
 
 
